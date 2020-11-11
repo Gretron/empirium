f00108ec4740708e008082b0083eb98de9b90fe8
TheodoreTsimiklis <theodoretsimiklis@gmail.com>
2020-11-10 20:11:27 -0500

    ADDED CODE (MOVEMENT CODE)

--- GameFile/MyWorld.java
+++ GameFile/MyWorld.java
@@ -1,4 +1,3 @@
-// WARNING: This file is auto-generated and any changes to it will be overwritten
 import lang.stride.*;
 import java.util.*;
 import greenfoot.*;
@@ -14,6 +13,6 @@ public class MyWorld extends World
      */
     public MyWorld()
     {
-        super(600, 400, 1);
+        super(1280, 720, 1);
     }
 }
